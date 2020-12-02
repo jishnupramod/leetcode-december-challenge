aa5d3b17750b13ff76d39823359a901aa1af8807
jishnupramod <jishnumagnanimous@gmail.com>
2020-12-02 19:54:25 +0530

    reservoir sampling method - algorithm R

--- 02_linked_list_random_node.cpp
+++ 02_linked_list_random_node.cpp
@@ -59,4 +59,48 @@ public:
  * Your Solution object will be instantiated and called as such:
  * Solution* obj = new Solution(head);
  * int param_1 = obj->getRandom();
+ *
+ *
+
+
+ 
+
+// Reservoir Sampling method (Algorithm R by Alan Waterman)
+ /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    ListNode* start;
+    Solution(ListNode* head) {
+        start = head;
+    }
+    
+    /** Returns a random node's value. */
+    int getRandom() {
+        ListNode* temp = start;
+        int scope = 1, currVal = 0;
+        while (temp) {
+            if (((double)rand() / (RAND_MAX)) < 1.0 / scope)
+                currVal = temp->val;
+            temp = temp->next;
+            ++scope;
+        }
+        return currVal;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
  */
